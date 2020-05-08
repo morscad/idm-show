0d15636e59335d46a9caef6cd51de734bc7771c7
Omar Faleh <omar@therefore.ca>
2020-05-08 17:24:19 -0400

    remove env files from the git repo

---
+++ src/pages/CalendarPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./ProjectsPage.scss";
+import MainLayout from "../layout/MainLayout";
+const ProjectsPage = () => {
+  return (
+    <MainLayout>
+      <div></div>
+    </MainLayout>
+  );
+};
+
+export default ProjectsPage;

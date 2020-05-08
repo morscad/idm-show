37b145e66f38f54eb48ae12c44d094728123f680
Omar Faleh <omar@therefore.ca>
2020-05-08 17:25:18 -0400

    add http request and router to the project entry point

--- src/App.js
+++ src/App.js
@@ -1,19 +1,70 @@
-import React, { useState } from "react";
-import "./App.scss";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { concat } from "lodash";
+
+import "./App.scss";
 import HomePage from "./pages/HomePage";
 import UserManager from "./services/UserManager";
+import api from "./services/apiService";
+import AboutPage from "./pages/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import CalendarPage from "./pages/CalendarPage";
 
-const MainContext = React.createContext({ users: []});
+export const MainContext = React.createContext({ projects: [] });
 
 const App = () => {
-  const [state, setState] = useState({users: [] });
+  const [state, setState] = useState({ projects: [] });
+  const [isInit, init] = useState(false);
+
+  const getAllProjects = async () => {
+    let projects = [];
+    const result = await api.sendRequest(
+      process.env.REACT_APP_AIRTABLE_URL.split("{-}").join(
+        process.env.REACT_APP_AIRTABLE_BASE_ID
+      ),
+      {},
+      "GET"
+    );
+    const { records, offset } = result;
+    projects = concat(projects, records);
+    if (!!offset && offset !== "") {
+      const result2 = await api.sendRequest(
+        `${process.env.REACT_APP_AIRTABLE_URL.split("{-}").join(
+          process.env.REACT_APP_AIRTABLE_BASE_ID
+        )}?offset=${offset}`,
+        {},
+        "GET"
+      );
+      projects = concat(projects, result2.records);
+      const { records2 } = result2;
+    }
+    setState({ projects: projects });
+    // console.log(projects);
+  };
+  useEffect(() => {
+    if (!isInit) {
+      init(true);
+      getAllProjects();
+    }
+  }, [isInit]);
   return (
     <MainContext.Provider value={[state, setState]}>
       <Router>
-            <HomePage context={MainContext} />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/about">
+            <AboutPage />
+          </Route>
+          <Route exact path="/projects">
+            <ProjectsPage />
+          </Route>
+          <Route exact path="/calendar">
+            <CalendarPage />
+          </Route>
+        </Switch>
       </Router>
-      <UserManager context={MainContext} />
     </MainContext.Provider>
   );
 };
